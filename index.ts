e29ac57dd7e3f66a8e6139c6730d8410df4bb7bd
influxion <nicholsjordon721@gmail.com>
2022-11-04 12:19:28 -0500

    First version

---
+++ index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env ts-node
+console.log(`
+Jordon Nichols
+
+Full-Stack Engineer focused on web based applications built with cutting edge security and the newest tech. Also working on cryptography and blockchain solutions on the web.
+
+Working with: TypeScript, Go, PHP, React, Angular, Node.js, GraphQL, Firebase, MySQL, MongoDB and many more!
+
+💡 LinkedIn: https://www.linkedin.com/in/jordon-nichols/
+📖 Github:   https://github.com/influxion
+
+`);
